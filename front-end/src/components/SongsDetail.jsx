b321db336a13e135a04d1a2e2667ecc0de25783f
D Jones <djones@Ds-MacBook-Air.local>
2023-10-31 18:57:42 -0400

    delete logic

--- front-end/src/components/SongsDetail.jsx
+++ front-end/src/components/SongsDetail.jsx
@@ -1,31 +1,48 @@
 import React from 'react'
 import { useState, useEffect } from "react";
+import { Link, useParams, useNavigate } from 'react-router-dom';
 import { Card } from 'react-bootstrap'
 const API = process.env.REACT_APP_API
 
 
 export default function SongsDetail() {
-    const [oneSong, setOneSong] = useState([]);
+    const [song, setSong] = useState([]);
+    let { index } = useParams();
+    let navigate = useNavigate();
 
     useEffect(() => {
         fetch(`${API}/songs`)
         .then((res) => res.json())
         .then((results) => {
          const { payload: songsData } = results.data;
-         setOneSong(songsData)   })
+         setSong(songsData)   })
         .catch((err) => {console.log("Fetch error")})
-       }, [])
+       }, [index, navigate])
+
+       const handleDelete = () => {
+        const httpOptions = { "method" : "DELETE"};
+        fetch(`${API}/songs/${index}`, httpOptions)
+        .then((res) => {
+            console.log(res)
+            alert("Hey you deleted a song!")
+        })
+        .catch((err) => console.log(err))
+       };
 
 
 
   return (
     <Card>
       <Card.Body>
-      <Card.Title>{oneSong.name}</Card.Title><span>{oneSong.is_favorite}</span>
+      <Card.Title>{song.name}</Card.Title><span>{song.is_favorite}</span>
+      <Link to={`/songs/${index}/edit`}>
+      <button>Edit</button>
+      </Link>  
+      <Card.Title>{song.name}</Card.Title><span>{song.is_favorite}</span><span><button onClick={handleDelete} >Delete</button></span>
       <Card.Text>
-        {oneSong.artist}
+        {song.artist}
       </Card.Text>
-      {oneSong.time}
+      {song.time}
       </Card.Body>
     </Card>
   )
