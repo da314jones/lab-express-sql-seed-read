9bb5b12f837416573787dc03c5c799c58427cbbd
D Jones <djones@Ds-MacBook-Air.mynetworksettings.com>
2023-11-03 23:53:14 -0400

    review form completed

--- front-end/src/components/Review.jsx
+++ front-end/src/components/Review.jsx
@@ -1,9 +1,69 @@
-import React from 'react'
+import React, { useState } from 'react'
+import ReviewForm from './ReviewForm';
 
-export default function Review() {
+export default function Review({
+    review, handleDelete, handleEdit 
+}) {
+    const [viewEditForm, setEditForm] = useState(false);
+
+    const toggleView = () => {
+        setEditForm(!viewEditForm);
+    };
+
   return (
-    <div>
-      
+//     <div className='review'>
+//       {viewEditForm ? (
+//         <>
+//           <ReviewForm
+//             reviewDetails={review}
+//             toggleView={toggleView}
+//             handleSubmit={handleEdit}
+//           />
+//           <button onClick={toggleView}>
+//             {viewEditForm ? "Cancel" : "Edit this review"}
+//           </button>
+//         </>
+//       ) : (
+//         <>
+//           <h4>
+//             {review.title}
+//             <span>{review.rating}</span>
+//           </h4>
+//           <h5>{review.reviewer}</h5>
+//           <p>{review.content}</p>
+//           <button onClick={toggleView}>
+//             {viewEditForm ? "Cancel" : "Edit this review"}
+//           </button>
+//           <button onClick={() => handleDelete(review.id)}>Delete</button>
+//         </>
+//       )}
+//     </div>
+//   )
+
+
+<div className='review'>
+      {viewEditForm ? (
+          <ReviewForm
+          reviewDetails={review}
+          toggleView={toggleView}
+          handleSubmit={handleEdit}
+        />
+        ) : (
+            <>
+          <h4>
+            {review.title}
+            <span> {review.rating}</span>
+          </h4>
+          <h5>{review.reviewer}</h5>
+          <p>{review.content}</p>
+        </>
+      )}
+      <button onClick={toggleView}>
+        {viewEditForm ? "Cancel" : "Edit this review"}
+      </button>
+      {!viewEditForm && (
+          <button onClick={() => handleDelete(review.id)}>Delete</button>
+          )}
     </div>
   )
-}
+        }
--- front-end/src/components/ReviewForm.jsx
+++ front-end/src/components/ReviewForm.jsx
@@ -1,9 +1,90 @@
-import React from 'react'
+import { useState, useEffect } from "react"
+import { useParams } from "react-router-dom"
 
-export default function ReviewForm() {
+export default function ReviewForm({ reviewDetails, handleSubmit, toggleView, children }) {
+    let { id } = useParams();
+const [review, setReview] = useState({
+    reviewer: "",
+    title: "",
+    content: "",
+    rating: "",
+    song_id: id,
+});
+
+const handleTextChange = (e) => {
+    setReview({ ...review, [e.target.id]: e.target.value });
+};
+
+useEffect(() => {
+    if (reviewDetails) {
+        setReview(reviewDetails);
+    }
+}, [id, reviewDetails]);
+
+const onSubmit = (e) => {
+    e.preventDefault();
+    handleSubmit(review, id);
+    if (reviewDetails) {
+        toggleView();
+    }
+    setReview({
+        reviewer: "",
+        title: "",
+        content: "",
+        rating: "",
+        song_id: id,
+    });
+    console.log("what is going on");
+}
+
   return (
-    <div>
-      
+    <div className="Edit">
+      {children}
+      <form onSubmit={onSubmit}>
+        <label htmlFor="reviewer">Name:</label>
+        <input
+          id="reviewer"
+          value={review.reviewer}
+          type="text"
+          onChange={handleTextChange}
+          placeholder="Your name"
+          required
+        />
+        <label htmlFor="title">Title:</label>
+        <input
+          id="title"
+          type="text"
+          required
+          value={review.title}
+          onChange={handleTextChange}
+        />
+        <label htmlFor="rating">Rating:</label>
+        <input
+          id="rating"
+          type="number"
+          name="rating"
+          min="0"
+          max="5"
+          step="1"
+          required
+          placeholder="Please enter value between 0 and 5"
+          value={review.rating}
+          onChange={handleTextChange}
+        />
+        <label htmlFor="content">Review:</label>
+        <textarea
+          id="content"
+          type="text"
+          name="content"
+          value={review.content}
+          placeholder="What do you think..."
+          onChange={handleTextChange}
+        />
+
+        <br />
+
+        <input type="submit" />
+      </form>
     </div>
-  )
+  );
 }
